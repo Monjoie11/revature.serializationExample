7c9e53a1a081630ce67043e840e769e8dcd4d878
Monjoie11 <jeff.franken@gmail.com>
2019-09-25 19:44:56 -0400

    initial commit

---
+++ com/revature/driver/Driver.java
@@ -0,0 +1,23 @@
+package com.revature.driver;
+
+import com.revature.dao.CarDAO;
+import com.revature.dao.CarDAOSerialization;
+import com.revature.pojo.Car;
+
+public class Driver {
+	
+	
+	private static CarDAO carDAO = new CarDAOSerialization();
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * Car car = new Car("Toypta", 3, 2004, "numbersandstuff", "white");
+		 * carDAO.createCar(car);;
+		 */
+		 
+		
+		System.out.println(carDAO.readCar("numbersandstuff"));
+	}
+        
+}
